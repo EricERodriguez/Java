b9ba0af9661a629884ef96deef6f4504fb7f3d78
Eric R <62493059+EricERodriguez@users.noreply.github.com>
2021-02-23 21:28:00 -0300

    visualizaciondefigconaste.java
    visualizacion de figuras con astericos ejer 2.18 2.19 2.20 2.21 2.22 2.23 Deitel & Deitel

---
+++ visualizaciondefigconaste.java
@@ -0,0 +1,35 @@
+// visualizacion de figuras con astericos ejer 2.18 2.19 2.20 2.21 2.22 2.23 Deitel & Deitel
+
+
+public class visualizaciondefigconaste
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("*********    ***      *       *    ");
+        System.out.println("*       *  *     *   ***     * *   ");
+        System.out.println("*       * *       * *****   *   *  ");
+        System.out.println("*       * *       *   *    *     * ");
+        System.out.println("*       * *       *   *   *       *");
+        System.out.println("*       * *       *   *    *     * ");
+        System.out.println("*       * *       *   *     *   *  ");
+        System.out.println("*       *  *     *    *      * *   ");
+        System.out.println("*********    ***      *       *    ");
+
+        System.out.printf("*%n**%n***%n****%n*****%n");
+
+        System.out.println("*");
+        System.out.println("***");
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("**");
+        
+        System.out.print("*");
+        System.out.println("***");
+        System.out.println("*****");
+        System.out.print("****");
+        System.out.println("**");
+
+        System.out.printf("%s%n%s%n%s%n", "*", "***", "*****");
+    }
+    
+}
